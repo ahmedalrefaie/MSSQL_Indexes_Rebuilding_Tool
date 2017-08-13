c7ddb5f4c25dc3151fef58403a6ca53d438d6853
AHMED ALREFAIE <AHMED ALREFAIE>
2017-08-14 01:54:32 +0200

    Add project files.

---
+++ DatabaseIndexesRebuilder/Form1.Designer.vb
@@ -0,0 +1,213 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.txtConnectionString = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtPercent = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtConnectionString
+        '
+        Me.txtConnectionString.Location = New System.Drawing.Point(154, 30)
+        Me.txtConnectionString.Name = "txtConnectionString"
+        Me.txtConnectionString.Size = New System.Drawing.Size(672, 20)
+        Me.txtConnectionString.TabIndex = 0
+        Me.txtConnectionString.Text = "Data Source=.;Initial Catalog=Master;Integrated Security=True;Connection Timeout=" &
+    "300"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(22, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(117, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Connection String  => "
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(876, 26)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Connect "
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.ComboBox1.Enabled = False
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(154, 66)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(268, 21)
+        Me.ComboBox1.TabIndex = 3
+        '
+        'Button2
+        '
+        Me.Button2.Enabled = False
+        Me.Button2.Location = New System.Drawing.Point(298, 110)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(261, 23)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Check indexes and Generate rebuild commands"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(22, 74)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(104, 13)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Select Database =>"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(22, 115)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(185, 13)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "min avg fragmentation in percent =>"
+        '
+        'txtPercent
+        '
+        Me.txtPercent.Location = New System.Drawing.Point(213, 112)
+        Me.txtPercent.Name = "txtPercent"
+        Me.txtPercent.Size = New System.Drawing.Size(70, 20)
+        Me.txtPercent.TabIndex = 7
+        Me.txtPercent.Text = "30"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(25, 159)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.Size = New System.Drawing.Size(926, 310)
+        Me.DataGridView1.TabIndex = 8
+        '
+        'Button3
+        '
+        Me.Button3.Enabled = False
+        Me.Button3.Location = New System.Drawing.Point(25, 475)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(271, 23)
+        Me.Button3.TabIndex = 9
+        Me.Button3.Text = "Run All of Commands one by one"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Enabled = False
+        Me.Button4.Location = New System.Drawing.Point(320, 475)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(271, 23)
+        Me.Button4.TabIndex = 10
+        Me.Button4.Text = "Run Selected Commands only"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(599, 115)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(19, 13)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "..."
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(25, 528)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(925, 95)
+        Me.ListBox1.TabIndex = 12
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(25, 509)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(60, 13)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "Errors log :"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(962, 635)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.txtPercent)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtConnectionString)
+        Me.Name = "Form1"
+        Me.Text = "MSSQL Server Indexes Rebuilder , Devloped by Ahmed Alrfaie "
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtConnectionString As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtPercent As TextBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label5 As Label
+End Class
---
+++ DatabaseIndexesRebuilder/Form1.vb
@@ -0,0 +1,137 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim con As New SqlConnection(txtConnectionString.Text)
+        Try
+            con.Open()
+            MsgBox("it's Working ", MsgBoxStyle.Information)
+            con.Close()
+            GetAllDatabases()
+        Catch ex As Exception
+            MsgBox("Error , It's not working :(", MsgBoxStyle.Critical)
+
+        End Try
+    End Sub
+
+    Function GetAllDatabases() As Boolean
+        Dim con As New SqlConnection(txtConnectionString.Text)
+
+        Dim da As New SqlDataAdapter("select name from  master.dbo.sysdatabases", con)
+        Dim dt As New DataTable
+        da.Fill(dt)
+        ComboBox1.DisplayMember = "name"
+        '    ComboBox1.ValueMember = ""
+        ComboBox1.DataSource = dt
+        ComboBox1.Enabled = True
+        Button2.Enabled = True
+        Return True
+    End Function
+
+    Function GetListOfIndexesRbuildCommands() As Boolean
+        Dim con As New SqlConnection(txtConnectionString.Text)
+
+        Dim da As New SqlDataAdapter(" Select 'USE " & ComboBox1.Text & "  ALTER INDEX   [' +  name   + '] ON ' +    OBJECT_NAME (a.object_id,database_id) + ' ReBuild'   as 'Rebuild Command' FROM " & ComboBox1.Text & ".sys.dm_db_index_physical_stats (DB_ID(N'" & ComboBox1.Text & "'),NULL, NULL, NULL, NULL) AS a, " & ComboBox1.Text & ".sys.indexes AS b       where a.object_id = b.object_id And a.index_id = b.index_id   And name Is Not null And avg_fragmentation_in_percent >= " & txtPercent.Text & "    order by avg_fragmentation_in_percent desc", con)
+        'TextBox1.Text = da.SelectCommand.CommandText
+        Dim dt As New DataTable
+        da.Fill(dt)
+        DataGridView1.DataSource = dt
+        Button3.Enabled = True
+        Button4.Enabled = True
+        Label4.Text = "There are : " & dt.Rows.Count & "  Indexes you should rebuild"
+        Return True
+    End Function
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        GetListOfIndexesRbuildCommands()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim C As Integer = 0
+
+        Try
+            For i = 0 To DataGridView1.Rows.Count - 1
+                Application.DoEvents()
+                Try
+                    PerformSQLCommand(DataGridView1.Rows(i).Cells(0).Value.ToString)
+                    C += 1
+
+                    '    Threading.Thread.Sleep(100)
+                    ListBox1.Items.Add(Now.ToLongTimeString & ":Done:" & DataGridView1.Rows(i).Cells(0).Value.ToString)
+
+                    Application.DoEvents()
+
+                Catch ex As Exception
+                    '  Me.Text = ex.Message
+                    ListBox1.Items.Add(Now.ToLongTimeString & ":" & ex.Message)
+                End Try
+            Next
+        Catch ex As Exception
+
+        End Try
+        MsgBox(C & " of indexs was rebuilded ", MsgBoxStyle.Information)
+        GetListOfIndexesRbuildCommands()
+    End Sub
+
+    Function PerformSQLCommand(SqlCommandToExecute As String) As Boolean
+        Dim con As New SqlConnection(txtConnectionString.Text)
+
+        Dim com As New SqlCommand(SqlCommandToExecute, con)
+        Try
+            com.CommandTimeout = 300
+            con.Open()
+
+            com.ExecuteNonQuery()
+        Catch ex As Exception
+            ' Me.Text = ex.Message
+            ListBox1.Items.Add(Now.ToLongTimeString & ":" & ex.Message)
+
+        Finally
+            con.Close()
+        End Try
+        Return True
+    End Function
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim C As Integer = 0
+        Try
+
+            For i = 0 To DataGridView1.Rows.Count - 1
+                If DataGridView1.Rows(i).Selected = True Then
+                    Application.DoEvents()
+                    Try
+                        PerformSQLCommand(DataGridView1.Rows(i).Cells(0).Value.ToString)
+                        C += 1
+                        '    Threading.Thread.Sleep(100)
+                        ListBox1.Items.Add(Now.ToLongTimeString & ":Done:" & DataGridView1.Rows(i).Cells(0).Value.ToString)
+
+                        Application.DoEvents()
+
+                    Catch ex As Exception
+                        '   Me.Text = ex.Message
+                        ListBox1.Items.Add(Now.ToLongTimeString & ":" & ex.Message)
+
+                    End Try
+                End If
+
+            Next
+        Catch ex As Exception
+
+        End Try
+        MsgBox(C & " of indexs was rebuilded ", MsgBoxStyle.Information)
+        If C >= 1 Then
+            GetListOfIndexesRbuildCommands()
+        End If
+    End Sub
+End Class
+
+
+
+'Select 'ALTER INDEX   [' +  name   + '] ON ' +    OBJECT_NAME (a.object_id,database_id) + ' ReBuild' --, avg_fragmentation_in_percent  FROM sys.dm_db_index_physical_stats (DB_ID(N'SMS_Authorization'),NULL, NULL, NULL, NULL) AS a, sys.indexes AS b       where a.object_id = b.object_id And a.index_id = b.index_id   And name Is Not null And avg_fragmentation_in_percent > 10   order by avg_fragmentation_in_percent desc
